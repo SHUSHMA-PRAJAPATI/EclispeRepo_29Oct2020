7ceccdacd35ae4643abb824f5e81439de35dd910
shushma <angel28021997@gmail.com>
2020-10-29 10:33:41 +0530

    branch 1 first  commit

---
+++ Project2/src/Customer.java
@@ -0,0 +1,10 @@
+
+public class Customer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("We have added customers to project 2");
+
+	}
+
+}
